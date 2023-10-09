19a53735534e37bdaffaa93e83b6590520b65238
Hayan Khan <hayankhanmehsood@gmail.com>
2023-10-10 00:00:04 +0500

    total fixed

--- src/components/CartComponent.jsx
+++ src/components/CartComponent.jsx
@@ -62,7 +62,7 @@ function CartComponent() {
           <div className="mt-6 h-full rounded-lg border bg-white p-6 shadow-md md:mt-0 md:w-1/3">
             <div className="mb-2 flex justify-between">
               <p className="text-gray-700">Subtotal</p>
-              <p className="text-gray-700">{total}</p>
+              <p className="text-gray-700">${total}</p>
             </div>
             <div className="flex justify-between">
               <p className="text-gray-700">Shipping</p>
